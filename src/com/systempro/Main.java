6969e29e7bdf384a28fe4f799342b864235a654e
Uros <karaleicuu@gmail.com>
2023-09-17 18:43:06 +0200

    linked list

--- src/com/systempro/Main.java
+++ src/com/systempro/Main.java
@@ -1,12 +1,13 @@
 package com.systempro;
 
 import com.systempro.collections.ArrayList;
+import com.systempro.collections.LinkedList;
 
 class Main{
     public static void main(String[] args) {
-        ArrayList list=new ArrayList();
+        LinkedList list=new LinkedList();
         for(int i=0;i<10;i++){
-            list.add(i);
+            list.addStart(i);
         }
         for(int i=0;i<10;i++){
             System.out.println(list.get(i));
--- src/com/systempro/collections/LinkedList.java
+++ src/com/systempro/collections/LinkedList.java
@@ -18,19 +18,35 @@ public class LinkedList {
         root=null;
     }
     public void addStart(int value){
-
+        root=new Node(value,root);
     }
     public void addEnd(int value){
-
+        if(root==null){
+            root=new Node(value);
+            return;
+        }
+        Node node=root;
+        while (node.next!=null)node=node.next;
+        node.next=new Node(value);
     }
     public void removeStart(){
-
+        root=root.next;
     }
     public void removeEnd(){
-
+        if(root==null||root.next==null){
+            root=null;
+            return;
+        }
+        Node node=root;
+        while(node.next.next!=null)node=node.next;
+        node.next=null;
     }
     public int get(int i){
-        return 0;
+        Node node=root;
+        for(int j=0;j<i;j++){
+            node=node.next;
+        }
+        return node.value;
     }
     public void insert(int value,int i){
 
