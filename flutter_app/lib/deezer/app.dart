ee5c88bfa9224e304e8179523c2614dfd0702b80
Uros <karaleicuu@gmail.com>
2024-03-31 19:59:37 +0200

    radi search deezer

--- flutter_app/lib/deezer/app.dart
+++ flutter_app/lib/deezer/app.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_app/deezer/utils.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -8,6 +11,22 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+  Future<void> getData()async{
+    var response=await Utils.search(type: SearchType.track, query: "eminem");
+    var json=jsonDecode(response.body);
+    var data=json['data'];
+    for(var entry in data){
+      var title=entry['title'];
+      var id=entry['id'];
+      print('title: $title\nid:$id\n\n');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
--- flutter_app/lib/deezer/utils.dart
+++ flutter_app/lib/deezer/utils.dart
@@ -1,15 +1,44 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Utils{
   static String api="https://api.deezer.com";
 
-  static String? search({
-    required String type,required String query}){
-    http.get(Uri.parse('$api/search/$type?q=$query'));
+  static Future<ApiResponse> search({
+    required SearchType type,
+    required String query
+  })async{
+    return apiRequest('get','$api/search/${type.value}?q=$query');
   }
 }
 enum SearchType{
   track(value:"track");
   const SearchType({required this.value});
   final String value;
+}
+class ApiResponse{
+  final int status;
+  final String body;
+  ApiResponse(this.status,this.body);
+}
+Future<ApiResponse> apiRequest(String type,String url,{Map<String,String>? headers,Object? body})async{
+  try{
+    switch(type){
+      case 'get':{
+        var response=await http.get(Uri.parse(url),headers: headers);
+        return ApiResponse(response.statusCode, response.body);
+      }
+      case 'post':{
+        var response=await http.post(Uri.parse(url),headers: headers,body: body);
+        return ApiResponse(response.statusCode, response.body);
+      }
+      default:{
+        print("[ERROR] unknown request type");
+        return ApiResponse(500, jsonEncode({"message":"unknown response type"}));
+      }
+    }
+
+  }catch(e){
+    return ApiResponse(500, jsonEncode(e));
+  }
 }
