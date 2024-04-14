da20a4fbb3eeb6522707f6c2ae117e6801581b5f
Uros <karaleicuu@gmail.com>
2024-04-14 19:59:18 +0200

    rade senzori

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_app/sensors/app.dart';
 
 void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ],
+  );
   runApp(const App());
 }
--- flutter_app/lib/sensors/app.dart
+++ flutter_app/lib/sensors/app.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -8,8 +11,73 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepOrangeAccent,
+          brightness: Brightness.dark
+        )
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(onPressed: (){},icon: Icon(Icons.menu),),
+          title: Text("AppBar"),
+          actions: [],
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              StreamBuilder(
+                stream: accelerometerEventStream(),
+                builder: (context,snapshot){
+                  if(snapshot.hasData){
+                    return Text(
+                      'x: ${snapshot.data?.x.toStringAsFixed(3)}'
+                          'y:${snapshot.data?.y.toStringAsFixed(3)}'
+                          'z:${snapshot.data?.z.toStringAsFixed(3)}'
+                    );
+                  }else{
+                    return Text("no accelerometer data");
+                  }
+                },
+              ),
+              StreamBuilder(
+                stream: gyroscopeEventStream(),
+                builder: (context,snapshot){
+                  if(snapshot.hasData){
+                    var d=snapshot.data!;
+                    return Text(
+                      'x: ${d.x.toStringAsFixed(3)}'
+                      'y: ${d.y.toStringAsFixed(3)}'
+                      'z: ${d.z.toStringAsFixed(3)}'
+                    );
+                  }else{
+                    return Text('no gyroscope data');
+                  }
+                },
+              ),
+              StreamBuilder(
+                stream: magnetometerEventStream(),
+                builder: (context,snapshot){
+                  if(snapshot.hasData){
+                    var d=snapshot.data!;
+                    return Text(
+                        'x: ${d.x.toStringAsFixed(3)}'
+                            'y: ${d.y.toStringAsFixed(3)}'
+                            'z: ${d.z.toStringAsFixed(3)}'
+                    );
+                  }else{
+                    return Text('no magnetometer data');
+                  }
+                },
+              ),
+            ],
+          ),
+        )
+      ),
+    );
   }
 }
